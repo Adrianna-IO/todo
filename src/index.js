aae93f5a26d97d6b95cc549173f745e6e669dc9e
Adrianna-IO <isom.adrianna@gmail.com>
2020-07-26 19:41:29 -0400

    connected database to server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
 const api = express();
+
+//create a mysql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password',
+    database: 'todo'
+});
+
+try {
+    connection.connect();
+   } catch (e) {
+    console.log('Oops. Connection to MySQL failed.');
+    console.log(e);
+   }
 
 // defaults to .get route
 api.use(express.static(__dirname + '/public'));
@@ -23,5 +39,15 @@ api.listen(3000, () => {
 api.post('/add', (req, res) => {
     console.log(req.body)
     res.send('It works!');
+
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+        if (error) return res.json({ error: error });
+
+      connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+         if (error) return res.json({ error: error });
+         
+      console.log(results);
+        });
+       });
 });
 
