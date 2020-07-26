d032e9f0b77f368e27dae8ea58adc1a347126b0a
Adrianna-IO <isom.adrianna@gmail.com>
2020-07-26 16:13:25 -0400

    connected todo input to api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const api = express();
 
+// defaults to .get route
 api.use(express.static(__dirname + '/public'));
+api.use(bodyParser.json());
 
 api.listen(3000, () => {
     console.log('API up and running!');
@@ -16,4 +19,9 @@ api.listen(3000, () => {
 // api.use((req, res, next) => {
 //     console.log('Hello');
 // });
+
+api.post('/add', (req, res) => {
+    console.log(req.body)
+    res.send('It works!');
+});
 
